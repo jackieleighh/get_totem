f60bde11fa97ced8930698c15c015472d8b7bb24
Jackie Harnish <jackieleighh@gmail.com>
2025-03-14 16:14:37 -0400

    basic site

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -24,15 +24,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Get Totem
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="mailto:jackieleighh@gmail.com"
         >
-          Learn React
+          Contact Us
         </a>
       </header>
     </div>
