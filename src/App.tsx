9aa6b3dd03bd3ac9c328bc1592fcf4bb41667f3b
Jackie Harnish <jackieleighh@gmail.com>
2025-03-14 15:59:04 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZqtE2YC-wuquNorOzIW5gNtD1ESuQ8is",
+  authDomain: "get-totem.firebaseapp.com",
+  projectId: "get-totem",
+  storageBucket: "get-totem.firebasestorage.app",
+  messagingSenderId: "258539474151",
+  appId: "1:258539474151:web:27d98aa4e1c1c032f72642",
+  measurementId: "G-01KG6BFET5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
